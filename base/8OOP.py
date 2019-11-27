4d8af7e213b6bb4050478962ab2a8c92f24c264d
phantom <chenliangxingji1@163.com>
2019-11-27 17:48:56 +0800

    keep

---
+++ base/8OOP.py
@@ -0,0 +1,294 @@
+# 使用__slots__
+# 我们想要限制实例的属性怎么办？比如，只允许对Student实例添加name和age属性。
+# 为了达到限制的目的，Python允许在定义class的时候，定义一个特殊的__slots__变量，来限制该class实例能添加的属性：
+
+class Student(object):
+    # __slots__ = ('name', 'age')  # 用tuple定义允许绑定的属性名称
+    pass
+
+
+s = Student()
+s.name = 'Michael'
+s.age = 25
+
+
+# s.score = 99 error
+
+# 由于'score'没有被放到__slots__中，所以不能绑定score属性，试图绑定score将得到AttributeError的错误。
+def set_age(self, age):
+    self.age = age
+
+
+from types import MethodType
+
+s.set_age = MethodType(set_age, s)  # error
+s.set_age(25)
+print(s.age)
+
+
+# 在绑定属性时，如果我们直接把属性暴露出去，虽然写起来很简单，但是，没办法检查参数，导致可以把成绩随便改：
+# Python内置的@property装饰器就是负责把一个方法变成属性调用的：
+class Student(object):
+
+    @property
+    def score(self):
+        return self._score
+
+    @score.setter
+    def score(self, value):
+        if not isinstance(value, int):
+            raise ValueError('score must be an integer!')
+        if value < 0 or value > 100:
+            raise ValueError('score must between 0 ~ 100!')
+        self._score = value
+
+
+s = Student()
+s.score = 60  # OK，实际转化为s.set_score(60)
+print(s.score)  # OK，实际转化为s.get_score()
+
+
+# s.score = 9999  #error
+# print(s.score)
+
+# 还可以定义只读属性，只定义getter方法，不定义setter方法就是一个只读属性：
+class Student(object):
+
+    @property
+    def birth(self):
+        return self._birth
+
+    @birth.setter
+    def birth(self, value):
+        self._birth = value
+
+    @property
+    def age(self):
+        return 2015 - self._birth
+
+
+# 上面的birth是可读写属性，而age就是一个只读属性，因为age可以根据birth和当前时间计算出来。
+
+# 定制类
+# 看到类似__slots__这种形如__xxx__的变量或者函数名就要注意，这些在Python中是有特殊用途的。
+#
+# __slots__我们已经知道怎么用了，__len__()方法我们也知道是为了能让class作用于len()函数。
+#
+# 除此之外，Python的class中还有许多这样有特殊用途的函数，可以帮助我们定制类。
+
+class Student(object):
+    def __init__(self, name):
+        self.name = name
+
+    def __str__(self):  # 只需要定义好__str__()方法，返回一个好看的字符串就可以了：
+        return 'Student object (name: %s)' % self.name
+
+
+# __str__
+print(Student('Michael'))
+
+
+# 而__repr__()返回程序开发者看到的字符串，也就是说，__repr__()是为调试服务的
+
+# __iter__
+# 如果一个类想被用于for ... in循环，类似list或tuple那样，
+# 就必须实现一个__iter__()方法，该方法返回一个迭代对象，然后，
+# Python的for循环就会不断调用该迭代对象的__next__()方法拿到循环的下一个值，直到遇到StopIteration错误时退出循环。
+
+class Fib(object):
+    def __init__(self):
+        self.a, self.b = 0, 1  # 初始化两个计数器a，b
+
+    def __iter__(self):
+        return self  # 实例本身就是迭代对象，故返回自己
+
+    def __next__(self):
+        self.a, self.b = self.b, self.a + self.b  # 计算下一个值
+        if self.a > 100000:  # 退出循环的条件
+            raise StopIteration()
+        return self.a  # 返回下一个值
+
+
+for n in Fib():
+    print(n)
+
+
+# __getitem__
+# Fib实例虽然能作用于for循环，看起来和list有点像，但是，把它当成list来使用还是不行，比如，取第5个元素：
+# Fib()[5] error
+
+# 要表现得像list那样按照下标取出元素，需要实现__getitem__()方法：
+
+class Fib(object):
+    def __getitem__(self, n):
+        a, b = 1, 1
+        for x in range(n):
+            a, b = b, a + b
+        return a
+
+
+f = Fib()
+print(f[100])
+
+
+# print(f[:6])
+# 对于Fib却报错。原因是__getitem__()传入的参数可能是一个int，也可能是一个切片对象slice，所以要做判断：
+class Fib(object):
+    def __getitem__(self, n):
+        if isinstance(n, int):  # n是索引
+            a, b = 1, 1
+            for x in range(n):
+                a, b = b, a + b
+            return a
+        if isinstance(n, slice):  # n是切片
+            start = n.start
+            stop = n.stop
+            if start is None:
+                start = 0
+            a, b = 1, 1
+            L = []
+            for x in range(stop):
+                if x >= start:
+                    L.append(a)
+                a, b = b, a + b
+            return L
+
+
+f = Fib()
+print(f[:5])
+
+
+# 但是没有对step参数作处理：也没有对负数作处理，所以，要正确实现一个__getitem__()还是有很多工作要做的。
+# 果把对象看成dict，__getitem__()的参数也可能是一个可以作key的object，例如str。
+#
+# 与之对应的是__setitem__()方法，把对象视作list或dict来对集合赋值。最后，还有一个__delitem__()方法，用于删除某个元素。
+
+# 正常情况下，当我们调用类的方法或属性时，如果不存在，就会报错
+# 要避免这个错误，除了可以加上一个score属性外，Python还有另一个机制，那就是写一个__getattr__()方法，动态返回一个属性。修改如下
+
+class Student(object):
+
+    def __init__(self):
+        self.name = 'Michael'
+
+    def __getattr__(self, attr):
+        if attr == 'score':
+            return 99
+# 当调用不存在的属性时，比如score，Python解释器会试图调用__getattr__(self, 'score')来尝试获得属性，这样，我们就有机会返回score的值
+
+s = Student()
+print(s.name)
+print(s.score)
+print(s.age)
+
+# 注意，只有在没有找到属性的情况下，才调用__getattr__，已有的属性，比如name，不会在__getattr__中查找。
+
+# 注意到任意调用如s.abc都会返回None，这是因为我们定义的__getattr__默认返回就是None。要让class只响应特定的几个属性，我们就要按照约定，抛出AttributeError的错误
+
+# __call__
+# 一个对象实例可以有自己的属性和方法，当我们调用实例方法时，我们用instance.method()来调用。能不能直接在实例本身上调用呢？在Python中，答案是肯定的。
+# 任何类，只需要定义一个__call__()方法，就可以直接对实例进行调用。请看示例：
+class Student(object):
+    def __init__(self, name):
+        self.name = name
+
+    def __call__(self):
+        print('My name is %s.' % self.name)
+s = Student('Michael')
+s()
+# 其实，更多的时候，我们需要判断一个对象是否能被调用，能被调用的对象就是一个Callable对象，比如函数和我们上面定义的带有__call__()的类实例：
+print(callable(s))
+
+#枚举
+from enum import Enum,unique
+Month = Enum('Month', ('Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'))
+for name, member in Month.__members__.items():
+    print(name, '=>', member, ',', member.value)
+    # value属性则是自动赋给成员的int常量，默认从1开始计数。
+
+@unique
+#@unique装饰器可以帮助我们检查保证没有重复值。
+class Weekday(Enum):
+    Sun = 0 # Sun的value被设定为0
+    Mon = 1
+    Tue = 2
+    Wed = 3
+    Thu = 4
+    Fri = 5
+    Sat = 6
+
+# 动态语言和静态语言最大的不同，就是函数和类的定义，不是编译时定义的，而是运行时动态创建的。
+# 比方说我们要定义一个Hello的class，就写一个hello.py模块：
+class Hello(object):
+    def hello(self, name='world'):
+        print('hello, %s.' % name)
+# 当Python解释器载入hello模块时，就会依次执行该模块的所有语句，执行结果就是动态创建出一个Hello的class对象，测试如下：
+# >>> from hello import Hello
+# >>> h = Hello()
+# >>> h.hello()
+# Hello, world.
+# >>> print(type(Hello))
+# <class 'type'>
+
+print(type(Hello))
+# type()函数可以查看一个类型或变量的类型，Hello是一个class，它的类型就是type，而h是一个实例，它的类型就是class Hello。
+h = Hello()
+print(type(h))
+
+
+def fn(self, name='world'):
+    print('Hello, %s.' % name)
+# type()函数既可以返回一个对象的类型，又可以创建出新的类型，比如，我们可以通过type()函数创建出Hello类，而无需通过class Hello(object)...的定义：
+Hello = type('Hello', (object,), dict(hello=fn))  # 创建Hello class
+h = Hello()
+h.hello()
+# 要创建一个class对象，type()函数依次传入3个参数：
+#
+# class的名称；
+# 继承的父类集合，注意Python支持多重继承，如果只有一个父类，别忘了tuple的单元素写法；
+# class的方法名称与函数绑定，这里我们把函数fn绑定到方法名hello上。
+
+# 通过type()函数创建的类和直接写class是完全一样的，因为Python解释器遇到class定义时，仅仅是扫描一下class定义的语法，然后调用type()函数创建出class。
+
+# metaclass
+# 除了使用type()动态创建类以外，要控制类的创建行为，还可以使用metaclass。
+#
+# metaclass，直译为元类，简单的解释就是：
+# 当我们定义了类以后，就可以根据这个类创建出实例，所以：先定义类，然后创建实例。
+#
+# 但是如果我们想创建出类呢？那就必须根据metaclass创建出类，所以：先定义metaclass，然后创建类。
+#
+# 连接起来就是：先定义metaclass，就可以创建类，最后创建实例。
+
+# metaclass是类的模板，所以必须从`type`类型派生：
+class ListMetaclass(type):
+    def __new__(cls, name, bases, attrs):
+        attrs['add'] = lambda self, value: self.append(value)
+        return type.__new__(cls, name, bases, attrs)
+
+# 有了ListMetaclass，我们在定义类的时候还要指示使用ListMetaclass来定制类，传入关键字参数metaclass：
+class MyList(list, metaclass=ListMetaclass):
+    pass
+# 当我们传入关键字参数metaclass时，魔术就生效了，它指示Python解释器在创建MyList时，要通过ListMetaclass.__new__()来创建，在此，我们可以修改类的定义，比如，加上新的方法，然后，返回修改后的定义。
+#
+# __new__()方法接收到的参数依次是：
+#
+# 当前准备创建的类的对象；
+#
+# 类的名字；
+#
+# 类继承的父类集合；
+#
+# 类的方法集合。
+L = MyList()
+L.add(1)
+print(L)
+# 而普通的list没有add()方法：
+
+# 但是，总会遇到需要通过metaclass修改类定义的。ORM就是一个典型的例子。
+#
+# ORM全称“Object Relational Mapping”，即对象-关系映射，就是把关系数据库的一行映射为一个对象，也就是一个类对应一个表，这样，写代码更简单，不用直接操作SQL语句。
+#
+# 要编写一个ORM框架，所有的类都只能动态定义，因为只有使用者才能根据表的结构定义出对应的类来。
+# 这里用到python的orm再回来看吧 https://www.liaoxuefeng.com/wiki/1016959663602400/1017592449371072
+
