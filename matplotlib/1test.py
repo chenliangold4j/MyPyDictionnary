e8f44bc0e80dc89c8172aa43f708652751e055ad
chenliang <chenliangxingji1@163.com>
2020-08-18 15:04:45 +0800

    fix

--- matplotlib/1test.py
+++ matplotlib/1test.py
@@ -1,7 +1,16 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+# numpy.linspace(start, stop, num=50, endpoint=True, retstep=False, dtype=None)
+#
+# 在指定的间隔内返回均匀间隔的数字。
+#
+# 返回num均匀分布的样本，在[start, stop]。
+#
+# 这个区间的端点可以任意的被排除在外。
+
 X = np.linspace(-np.pi, np.pi, 256, endpoint=True)
+print(X)
 C, S = np.cos(X), np.sin(X)
 
 plt.plot(X, C)
