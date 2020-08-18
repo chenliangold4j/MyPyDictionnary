d8853213ad88df65750dc66614320714e9a0bfc0
chenliang <chenliangxingji1@163.com>
2020-08-18 18:07:13 +0800

    fix

--- matplotlib/5pyplot2.py
+++ matplotlib/5pyplot2.py
@@ -6,7 +6,8 @@ data = {'a': np.arange(50),
         'd': np.random.randn(50)}
 data['b'] = data['a'] + 10 * np.random.randn(50)
 data['d'] = np.abs(data['d']) * 100
-
+# matplotlib.pyplot.scatter(x, y, s=None, c=None, marker=None, cmap=None, norm=None, vmin=None, vmax=None, alpha=None, linewidths=None,
+#                           verts=<deprecated parameter>, edgecolors=None, *, plotnonfinite=False, data=None, **kwargs)[source]
 plt.scatter('a', 'b', c='c', s='d', data=data)
 plt.xlabel('entry a')
 plt.ylabel('entry b')
---
+++ matplotlib/5pyplot4.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+import numpy as np
+#
+# lines = plt.plot(x1, y1, x2, y2)
+# # use keyword args
+# plt.setp(lines, color='r', linewidth=2.0)
+# # or MATLAB style string value pairs
+# plt.setp(lines, 'color', 'r', 'linewidth', 2.0)
