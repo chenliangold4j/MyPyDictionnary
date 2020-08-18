9a021469a00eddc2dd7b5e1fc9a8dd796b7dd9f8
chenliang <chenliangxingji1@163.com>
2020-08-18 20:01:09 +0800

    fix

--- sympy/4sympy_def_sym.py
+++ sympy/4sympy_def_sym.py
@@ -18,9 +18,7 @@ expr3 = 2 * i * j + i * j
 print(expr3)
 # 该程序显示了三种在 SymPy 中定义符号的方法。
 
-
 # SymPy 会自动将表达式转换为规范形式。 SymPy 仅执行廉价操作； 因此，表达式可能无法求值为最简单的形式。
-expr = b*a + -4*a + b + a*b + 4*a + (a + b)*3
+expr = b * a + -4 * a + b + a * b + 4 * a + (a + b) * 3
 
 print(expr)
-
